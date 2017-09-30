f93052d8f98c49d89b1dbcd55bfce29b67416565
paozitao <lt5132908@gmail.com>
2017-09-30 19:50:02 +0800

    submit

---
+++ JavaScript/myjs.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 刘涛 on 2017/7/14.
+ */
+var myFocus;
+myFocus.set({
+    id:'box',
+    time:1.5,
+    wrap:false
+})
+function meauon()
+{
+    var x=document.getElementById("zime")
+    x.style.display="block";
+}
+function meauout() {
+    var x = document.getElementById("zime")
+    x.style.display = "none";
+}
+
